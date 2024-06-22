333f7374374717e627ed81e9b5199d660f784614
Petro Kobevko <kobevkopi@gmail.com>
2024-06-22 11:43:37 +0300

    Initial commit

---
+++ src/test/java/dev/pkobevko/SpringitApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.pkobevko;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringitApplicationTests {
+    public SpringitApplicationTests() {
+    }
+
+    @Test
+    public void contextLoads() {
+    }
+}
