d085adc479a9e46e0a76ef34b33c724efc3fe3ee
Petro Kobevko <pkobevko@protonmail.com>
2024-08-17 12:37:40 +0300

    Commented out command line runner

--- src/main/java/dev/pkobevko/SpringitApplication.java
+++ src/main/java/dev/pkobevko/SpringitApplication.java
@@ -7,7 +7,6 @@ import dev.pkobevko.repository.LinkRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -17,7 +16,7 @@ public class SpringitApplication {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
+//    @Bean
     CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
         return args -> {
             Link link = new Link("Google", "https://google.com");
